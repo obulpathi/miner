b9bba6e40084e9a45311fcf3601ba70f98b10a4a
Obulapathi N Challa <obulpathi@gmail.com>
2013-11-15 19:46:33 -0500

    Adding commands for Google Cloud

---
+++ google.sh
@@ -0,0 +1,11 @@
+# TODO:
+# Create a presistent bootsisk, install software and set everything,
+# just launch and be done ... 
+# check periodicall and be done ... 
+# install git, vim and make
+gcutil auth --project=meteor-shower
+gcutil getproject --project=meteor-shower --cache_flag_values
+# add any firewall if necessary
+# gcutil addinstance <instance-name> --persistent_boot_disk
+gcutil addinstance machine_name --persistent_boot_disk
+gcutil getinstance machine_name
