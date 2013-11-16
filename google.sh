50149c778a33f353c9cad683a235e041850c72bf
Obulapathi N Challa <obulpathi@gmail.com>
2013-11-15 19:49:40 -0500

    misc

--- google.sh
+++ google.sh
@@ -3,6 +3,7 @@
 # just launch and be done ... 
 # check periodicall and be done ... 
 # install git, vim and make
+# have to use screen so that conneciton will not terminate
 gcutil auth --project=meteor-shower
 gcutil getproject --project=meteor-shower --cache_flag_values
 # add any firewall if necessary
